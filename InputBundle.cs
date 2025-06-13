55f03e2798ef34ceb8ae78eb8d1bfbd914d0c513
aleksey.pilov <aleksey.pilov@yandex.ru>
2025-06-13 13:52:47 +0300

    loop stages

---
+++ InputBundle.cs
@@ -0,0 +1,18 @@
+﻿using Engine.Core;
+using Engine.Input.Config;
+using GignerEngine.DiContainer;
+
+namespace Engine.Input;
+
+public class InputBundle : IBundle
+{
+    public void InstallBindings(DiBuilder builder)
+    {
+        builder.Bind<InputStage>();
+    }
+
+    public void Configure(string c, IReadonlyDiContainer diContainer)
+    {
+        var config = InputConfig.Load(c);
+    }
+}
---
+++ InputStage.cs
@@ -0,0 +1,17 @@
+﻿using Engine.Core.Stages;
+
+namespace Engine.Input;
+
+public class InputStage : IStage
+{
+    public Type[] Before { get; set; } = [typeof(LogicStage)];
+    public Type[] After { get; set; } = [];
+
+    public void Start()
+    {
+    }
+
+    public void Update(float dt)
+    {
+    }
+}
